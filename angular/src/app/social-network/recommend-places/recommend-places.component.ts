b331bd0226cb982eeba7fae61a8215d4160ab1ec
Vistark <services.vistark@gmail.com>
2021-06-16 21:15:40 +0700

    - Trang người dùng thiếu địa điểm và yêu thích

---
+++ angular/src/app/social-network/recommend-places/recommend-places.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recommend-places',
+  templateUrl: './recommend-places.component.html',
+  styleUrls: ['./recommend-places.component.css']
+})
+export class RecommendPlacesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
