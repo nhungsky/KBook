81ad932b8526e05e8248b95c65464ec4a2955d32
Vistark <services.vistark@gmail.com>
2021-07-14 23:02:56 +0700

    ahihi

--- aspnet-core/src/ASPCore.Angular.Application/PostComments/Dto/PostCommentDto.cs
+++ aspnet-core/src/ASPCore.Angular.Application/PostComments/Dto/PostCommentDto.cs
@@ -6,16 +6,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abp.Domain.Entities.Auditing;
+using ASPCore.Angular.Users.Dto;
 
 namespace ASPCore.Angular.PostComments.Dto
 {
     [AutoMapFrom(typeof(PostComment))]
     [AutoMapTo(typeof(PostComment))]
-    public class PostCommentDto : EntityDto
+    public class PostCommentDto : AuditedEntityDto
     {
         public int PostId { get; set; }
         public string ImagePath { get; set; }
         public string Comment { get; set; }
-
+        public UserDto User { get; set; }
     }
 }
--- aspnet-core/src/ASPCore.Angular.Application/PostComments/PostCommentAppService.cs
+++ aspnet-core/src/ASPCore.Angular.Application/PostComments/PostCommentAppService.cs
@@ -6,15 +6,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abp.Application.Services.Dto;
+using ASPCore.Angular.Authorization.Users;
+using ASPCore.Angular.Users.Dto;
 
 namespace ASPCore.Angular.PostComments
 {
     public class PostCommentAppService : AsyncCrudAppService<PostComment, PostCommentDto>, IPostCommentAppService
     {
-        public PostCommentAppService(IRepository<PostComment> repo) : base(repo) { }
+        private readonly IRepository<User, long> _userRepository;
+
+        public PostCommentAppService(IRepository<PostComment> repo, IRepository<User, long> userRepository) : base(repo)
+        {
+            _userRepository = userRepository;
+        }
+
         public async Task<int> Count()
         {
             return await Repository.CountAsync();
+        }
+
+        public override async Task<PagedResultDto<PostCommentDto>> GetAllAsync(PagedAndSortedResultRequestDto input)
+        {
+            var result = await base.GetAllAsync(input);
+            for (int i = 0; i < result.Items.Count; i++)
+            {
+                if (result.Items[i].CreatorUserId is > 0)
+                {
+                    var user = await _userRepository.GetAsync(result.Items[i].CreatorUserId.Value);
+                    if (user != null)
+                    {
+                        result.Items[i].User = ObjectMapper.Map<UserDto>(user);
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
